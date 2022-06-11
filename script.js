be48dc79b5ec5e084add7b770c16d49e5fb71b96
xaiur <syabilkahfialfatih@gmail.com>
2022-06-12 06:13:41 +0800

    latihan try & catch

--- script.js
+++ script.js
@@ -7,17 +7,33 @@
 // coba.then(() => console.log(coba));
 
 function cobaPromise() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('selesai');
-    }, 2000);
+  return new Promise((resolve, reject) => {
+    const waktu = 5000;
+    if (waktu < 5000) {
+      setTimeout(() => {
+        resolve('selesai');
+      }, waktu);
+    } else {
+      reject('kelamaan!');
+    }
   });
 }
+
 // const coba = cobaPromise();
-// coba.then(() => console.log(coba));
+// coba.then(() => console.log(coba)).catch(() => console.log(coba));
+
+// async function cobaAsync() {
+//   const coba = await cobaPromise();
+//   console.log(coba);
+// }
+// cobaAsync();
 
 async function cobaAsync() {
-  const coba = await cobaPromise();
-  console.log(coba);
+  try {
+    const coba = await cobaPromise();
+    console.log(coba);
+  } catch (err) {
+    console.error(err);
+  }
 }
 cobaAsync();
